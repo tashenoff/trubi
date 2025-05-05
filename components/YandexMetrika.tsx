b2d8d106a014be62a0ee8837d9a4eced14f0dd91
tashenoff <kail.dead.kd@gmail.com>
2025-05-05 23:45:21 +0600

    fix: исправлены ошибки типизации в YandexMetrika

--- components/YandexMetrika.tsx
+++ components/YandexMetrika.tsx
@@ -8,14 +8,30 @@ declare global {
   }
 }
 
+type YandexMetrikaWindow = Window & {
+  [key: string]: any;
+}
+
 export default function YandexMetrika() {
   useEffect(() => {
-    // @ts-ignore
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date().getTime();
-    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    (function(m: YandexMetrikaWindow, e: Document, t: string, r: string, i: string, k: any, a: any) {
+      (m[i] as any) = (m[i] as any) || function() {
+        ((m[i] as any).a = (m[i] as any).a || []).push(arguments);
+      };
+      (m[i] as any).l = 1 * new Date().getTime();
+      
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) return;
+      }
+      
+      k = e.createElement(t);
+      a = e.getElementsByTagName(t)[0];
+      k.async = 1;
+      k.src = r;
+      if (a && a.parentNode) {
+        a.parentNode.insertBefore(k, a);
+      }
+    })(window as YandexMetrikaWindow, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym", undefined, undefined);
 
     window.ym(101621389, "init", {
       clickmap: true,
